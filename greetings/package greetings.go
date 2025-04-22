99ae6f0118298ca547792cdfbeed54a7af185027
Staphnie Hung <staphnie-hung@hotmail.com>
2025-04-22 15:45:12 +0200

    add a bunch of random predefined greeting msgs

--- greetings/package greetings.go
+++ greetings/package greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 //Hello returns a greeting for the named person. 
@@ -13,9 +14,26 @@ func Hello(name string) (string, error) {
 		return "", errors.New("No name was given")
 	}
 
-	//if it was received, return a value that embeds the name in a greeting msg.
-
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	//Create a msg using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
 
 	return message, nil
+}
+
+//randomFormat returns on of a set of greeting msgs.
+//returned msg is selected at random.
+
+func randomFormat() string {
+	//A slice of message format. 
+
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Wassup big homie, %v!!",
+	}
+
+	//Return a randomly selected message format by specifying
+	// a random index for the slice of formats. 
+
+	return formats[rand.Intn(len(formats))]
 }
